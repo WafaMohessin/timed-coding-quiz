a9527b1e199df3db9b29d440a5e546747ad795e7
Wafa Mohessin <wafamohessin@gmail.com>
2021-08-21 20:56:09 -0400

    fixing code

--- script.js
+++ script.js
@@ -1,95 +1,3 @@
-var startButton = document.getElementById ('start-btn')
-var nextButton = document.getElementById ('next-btn')
-var questionContainer = document.getElementById ('question-container')
-
-var questionElement = document.createElement('question')
-var answerButtonElement= document.getElementById('answer-btn') 
-
-var shuffleQuestion, currentQuestionIndex 
-
-startButton.addEventListener ('click',startGame)
-nextButton.addEventListener ('click',() => {
-    currentQuestionIndex++
-})
-
-function startGame() {
-    console.log ('started')
-    startButton.classList.add ( 'hide')
-    shuffleQuestion=question.sort (() => Math.random () - .5)
-    currentQuestionIndex = 0
-    questionContainer.classList.remove ('hide')
-
-    setNextQuestion()
-}
-
-function setNextQuestion() {
-    resetState()
-    showingQuestion(shuffleQuestion [currentQuestionIndex])
-}
-
-
-function showQuestion(question){
-    
-    questionElement.innertext = question.question
-    question.answer.forEach (answer => {
-        var button = document.createElement('button')
-        button.classList.add ( 'btn')
-
-        if (answer.correct){
-            button.dataset.correct =answer.correct
-        }
-        button.addEventListener('click', selectedAnswer)
-        answerButtonElement.appendChild(button)
-    })
-
-}
-
-function resetState(){
-    nextButton.classList.add ( 'hide')
-
-    while(answerButtonElement.firstChild){
-        answerButtonElement.removeChild
-
-        (answerButtonElement.firstChild)
-    }
-
-
-}
-
-function selectedQuestion(e){
-    
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    
-    setStatusClass =(document.body.correct)
-   
-    Array.from(answerButtonElement.children).forEach(button => {
-        setStatusClass(button, button,dataset.correct)
-    })
-
-    if (shuffleQuestion.length > currentQuestionIndex +1){
-
-     nextButton.classList.remove('hide')
-    } else {
-        startButton.innerText='Restart'
-        startButton.classList.remove('hide')
-    }
-}
-
-/*function setStatusClass(element, correct){
-    clearStatusClass(element)
-        if (correct){
-            element.classList.add ( 'correct')
-        } else{
-            element.classList.add ( 'wrong')
-        }
-}*/
-
-function clearStatusClass(element){
-    element.classList.add ( 'correct')
-    element.classList.add ( 'wrong')
-}
-
 
 var questions = [
     {
@@ -130,3 +38,85 @@ var questions = [
         ]
     },
 ]
+
+
+var startButton = document.getElementById ('start-btn')
+var nextButton = document.getElementById ('next-btn')
+var questionContainer = document.getElementById ('question-container')
+
+var questionElement = document.createElement('question')
+var answerButtonElement= document.getElementById('answer-btn') 
+
+startButton.addEventListener ('click',startGame)
+nextButton.addEventListener ('click',() => {
+    currentQuestionIndex++
+})
+
+function startGame() {
+    console.log ('started')
+    startButton.classList.add ( 'hide')
+    currentQuestionIndex = 0
+    questionContainer.classList.remove ('hide')
+
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showingQuestion(currentQuestionIndex)
+}
+
+
+function showQuestion(question){
+    
+    questionElement.innertext = question.question
+    question.answer.forEach (answer => {
+        var button = document.createElement('button')
+        button.classList.add ( 'btn')
+
+        if (answer.correct){
+            button.dataset.correct =answer.correct
+        }
+        button.addEventListener('click', selectedAnswer)
+        answerButtonElement.appendChild(button)
+    })
+
+}
+
+function selectQuestion(e){
+    var selectButton = e.target
+    var correct = selectedButton.dataset.correct
+    
+    setStatusClass =(document.body.correct)
+    Array.from(answerButtonElement.children).forEach(button => {
+        setStatusClass(button, button,dataset.correct)
+    })
+
+    if (currentQuestionIndex){
+
+     nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText='Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+/*function setStatusClass(element, correct){
+    clearStatusClass(element)
+        if (correct){
+            element.classList.add ( 'correct')
+        } else{
+            element.classList.add ( 'wrong')
+        }
+}*/
+
+function clearStatusClass(element){
+    element.classList.add ( 'correct')
+    element.classList.add ( 'wrong')
+}
+
+
+
+
+
+
+
